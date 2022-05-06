c39c937799f19e1c38ff752d4b4c9afe365144d2
Mohib Shah <mohibs035@gmail.com>
2022-05-06 10:32:24 -0400

    Time: 189 ms (19.72%), Space: 16.7 MB (31.07%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,4 +1,14 @@
 class Solution:
+    def explore(self,graph,node,visited):
+        if node in visited:
+            return False
+        
+        visited.add(node)
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                self.explore(graph,neighbor,visited)
+        return True
+                
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         count = 0
         
@@ -8,22 +18,14 @@ class Solution:
             graph[i].append(j)
             graph[j].append(i)
         
-        def explore(node):
-            visited.add(node)
-        
-            for neighbor in graph[node]:
-                if neighbor not in visited:
-                    explore(neighbor)
-        
         visited = set()
         
         for i in range(n):
             if i not in visited:
-                explore(i)
+                self.explore(graph,i,visited)
                 count += 1
         return count
     
-
         
     
         
