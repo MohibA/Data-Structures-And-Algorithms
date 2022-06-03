ad100c0a05c70f0d9951ac81fa5ccf90fb86829a
Mohib Shah <mohibs035@gmail.com>
2022-06-03 15:08:16 -0400

    Time: 36 ms (81.89%), Space: 13.8 MB (83.50%) - LeetHub

---
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
@@ -0,0 +1,30 @@
+class Solution:
+    def tictactoe(self, moves: List[List[int]]) -> str:
+        a,b = [0] *8, [0] *8
+        
+        for index in range(len(moves)):
+            row,col = moves[index]
+            
+            if index % 2 == 0:
+                player = a
+            else:
+                player = b
+            
+            player[row] += 1
+            player[col + 3] += 1
+
+            if row == col:
+                player[6] += 1
+            if row == 2 - col:
+                player[7] += 1
+        for i in range(len(a)):
+            if a[i] == 3:
+                return "A"
+            elif b[i] == 3:
+                return "B"
+        if len(moves) == 9:
+            return "Draw"
+        else:
+            return "Pending"
+                
+        
