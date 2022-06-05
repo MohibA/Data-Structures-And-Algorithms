c0079e5ff7c787f3d30e67f7366115b71a52c928
Mohib Shah <mohibs035@gmail.com>
2022-06-04 21:36:48 -0400

    Time: 99 ms (61.02%), Space: 21.6 MB (50.54%) - LeetHub

---
+++ 510-inorder-successor-in-bst-ii/510-inorder-successor-in-bst-ii.py
@@ -0,0 +1,36 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def inorderSuccessor(self, node: 'Node') -> 'Optional[Node]':
+        curr_node = node
+        root_node = None
+        successor = None
+        
+        while curr_node:
+            if curr_node.parent is not None:
+                root_node = curr_node.parent
+                curr_node = curr_node.parent
+            else:
+                root_node = curr_node
+                break      
+        print(root_node.val)
+        while root_node:
+            if node.val >= root_node.val:
+                root_node = root_node.right
+            else:
+                successor = root_node
+                root_node = root_node.left
+        return successor
+        
+            
+            
+        
+        
