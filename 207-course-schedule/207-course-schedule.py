2763371603ce59e1c049f079c5772e423b7680fd
Mohib Shah <mohibs035@gmail.com>
2022-06-12 11:41:41 -0400

    Time: 189 ms (19.95%), Space: 16.9 MB (51.16%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,4 +1,20 @@
 class Solution:
+    def explore(self,courseMap,node,visited):
+        if node in visited:
+            return False
+        if courseMap[node] == []:
+            return True
+        
+        visited.add(node)
+        
+        for prereq in courseMap[node]:
+            if not self.explore(courseMap,prereq,visited):
+                return False
+        visited.remove(node)
+        courseMap[node] = []
+        
+        return True
+    
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         courseMap = defaultdict(list)
         visited = set()
@@ -6,24 +22,12 @@ class Solution:
         for i,j in prerequisites:
             courseMap[i].append(j)
         
-        #visited Set
-        def dfs(course):
-            if course in visited:
-                return False
-            if courseMap[course] == []:
-                return True
-            
-            visited.add(course)
-            for prereq in courseMap[course]:
-                if not dfs(prereq): return False
-            
-            visited.remove(course)
-            courseMap[course] = []
-            
-            return True
-        
         for course in range(numCourses):
-            if not dfs(course): return False
-        
+            if not self.explore(courseMap,course,visited):
+                return False
+            
         return True
+    
+    
+            
             
