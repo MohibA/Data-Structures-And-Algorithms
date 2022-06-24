32ac69786aee458d98a470baf619164144230eea
Mohib Shah <mohibs035@gmail.com>
2022-06-23 21:13:47 -0400

    Time: 126 ms (99.31%), Space: 15.1 MB (76.06%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,18 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        pointer = 0
-        
-        while pointer < len(nums):
-            num = nums[pointer]
-            
-            if num < len(nums) and num != pointer:
-                nums[pointer],nums[num] = nums[num],nums[pointer]
-            else:
-                pointer += 1
+        result = len(nums)
         
         for i in range(len(nums)):
-            if nums[i] != i:
-                return i
-        
-        return len(nums)
+            result += (i - nums[i])
+        return result
+    
+    
+    
                 
