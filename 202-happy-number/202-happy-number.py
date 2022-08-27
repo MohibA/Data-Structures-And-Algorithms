57e33024acf8729c30dcdceb404e44875eb3c9d8
Mohib Shah <mohibs035@gmail.com>
2022-08-27 10:00:32 -0400

    Time: 47 ms (68.68%), Space: 13.9 MB (14.75%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,17 +1,18 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        def get_next(number):
+        def getNext(num):
             total = 0
-            while number > 0:
-                number, digit = divmod(number,10)
+            
+            while num:
+                num,digit = divmod(num,10)
                 total += digit **2
             return total
         
-        slow_pointer = n
-        fast_pointer = get_next(n)
+        slow = n
+        fast = getNext(n)
         
-        while fast_pointer != 1 and slow_pointer != fast_pointer:
-            slow_pointer = get_next(slow_pointer)
-            fast_pointer = get_next(get_next(fast_pointer))
-        return fast_pointer == 1
+        while fast != 1 and slow != fast:
+            slow = getNext(slow)
+            fast = getNext(getNext(fast))
+        return fast == 1
         
